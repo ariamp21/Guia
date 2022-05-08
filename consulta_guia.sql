6eb876167ca62ec64683cdb54daf66db8cb18ca8
ariamp21 <48859269+ariamp21@users.noreply.github.com>
2022-05-07 23:30:03 -0500

    Update consulta_guia.sql
    Modificación tabla clientes.

--- consulta_guia.sql
+++ consulta_guia.sql
@@ -3,22 +3,22 @@ CREATE TABLE CLIENTES(
 ID INT AUTO_INCREMENT AUTO_INCREMENT KEY,
 NOMBRE varchar(25),
 PAIS varchar(25),
-CATEGORIA varchar(30),
-NUM_VENTA int
+CATEGORIA varchar(30)
 );
 
 INSERT INTO CLIENTES (ID,NOMBRE,PAIS,CATEGORIA,NUM_VENTA)
-VALUES(1, 'Ariam Aranda Reyes', 'Chile', 'Libros', 3),
-	  (2, 'Amaru Fernandez Durán', 'Chile', 'Tecnología',6),
-	  (3, 'Diana López Áviles','Alemania','Robótica',10),
-	  (4, 'Constanza Tobar Riquelme','China','Música',7),
-	  (5,'Claudia Cordero Solis', 'Alemania', 'Entretención',9),
-	  (6, 'Victoria Aravena Opazo','China','Eco',5),
-	  (7, 'Sally Aranda Reyes','España','Accesorios',9),
-	  (8, 'Cecilia Reyes Vargas','Italia','Entretención',10),
-	  (9, 'Ada Wynn Lunt','Mexico','Libros',5),
-	  (10, 'Alicia Aranda Reyes','Chile','Mascotas',11),
-	  (11,'Hilda Vicencio Serey','Perú','Entretención',4);
+VALUES(1, 'Ariam Aranda Reyes', 'Chile', 'Libros'),
+	  (2, 'Amaru Fernandez Durán', 'Chile', 'Tecnología'),
+	  (3, 'Diana López Áviles','Alemania','Robótica'),
+	  (4, 'Constanza Tobar Riquelme','China','Música'),
+	  (5,'Claudia Cordero Solis', 'Alemania', 'Entretención'),
+	  (6, 'Victoria Aravena Opazo','China','Eco'),
+	  (7, 'Sally Aranda Reyes','España','Accesorios'),
+	  (8, 'Cecilia Reyes Vargas','Italia','Entretención'),
+	  (9, 'Ada Wynn Lunt','Mexico','Libros'),
+	  (10, 'Alicia Aranda Reyes','Chile','Mascotas'),
+	  (11,'Hilda Vicencio Serey','Perú','Entretención');
+	 
 	  
 #####################################
 
